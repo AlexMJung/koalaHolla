7bb83508f57016b87b65936b94c175dc643801dc
Alex Jung <alexandermjung89@gmail.com>
2017-08-11 16:35:53 -0500

    attempted hard mode;2C

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -18,8 +18,26 @@ $( document ).ready( function(){
     // call saveKoala with the new obejct
     saveKoala( newKoala );
   }); //end addButton on click
-}); // end doc ready
 
+
+$('#viewKoalas').on('click','.delete', function(){
+console.log('delete button works, Holla!!!!');
+  var koalaID = $(this).parent().parent().data().id;
+  //var newMessage = $(this).val();
+  console.log(koalaID);
+  
+$.ajax ({
+  method: 'DELETE',
+  url: '/koalas/' + koalaID,
+  success: function(response){
+    clearKoala();
+    getKoalas();
+  }
+})
+
+});//end of deleteButton
+
+}); // end doc ready
 function getKoalas(){
   console.log( 'in getKoalas' );
   // ajax call to server to get koalas
@@ -30,12 +48,17 @@ function getKoalas(){
       console.log( 'got some koalas: ', response );
       for (var i = 0; i < response.length; i++) {
         var koalaKnapper = response[i];
-      $('#viewKoalas').prepend(
-        '<tr><td>' + koalaKnapper.name + '</td>' +
+        var $koalaTr = $('#viewKoalas')
+        //$koalaTr.children.data('id', koalaKnapper.id);
+        $koalaTr.append(
+        '<tr id=' + koalaKnapper.id +'>' +
+        '<td>' + koalaKnapper.name + '</td>' +
         '<td>' + koalaKnapper.age + '</td>' +
         '<td>' + koalaKnapper.gender + '</td>' +
         '<td>' + koalaKnapper.ready_for_transfer + '</td>' +
-        '<td>' + koalaKnapper.notes + '</td><tr>'
+        '<td>' + koalaKnapper.notes + '</td>' +
+        '<td> <button class="delete">Delete</button>'+
+        '</tr>'
       )//end of prepend
       }//end of for loop
     } // end success
